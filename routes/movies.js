916262b0ddaea43e0513a11e0a5eb9dbcbe53cc6
Ekaterina Pozhidaeva <kateworks@yandex.ru>
2021-02-19 15:22:10 +0300

    fix: защитить авторизацией маршрут по умолчанию

--- routes/movies.js
+++ routes/movies.js
@@ -1,18 +1,10 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
 
-const {
-  validateMovie, validateMovieId,
-} = require('../middlewares/validation');
+const { validateMovie, validateMovieId } = require('../middlewares/validation');
+const { getMovies, createMovie, deleteMovie } = require('../controllers/movies.js');
 
-const {
-  getMovies, createMovie, deleteMovie,
-} = require('../controllers/movies.js');
-
-router.get('/movies', auth, getMovies);
-
-router.post('/movies', auth, validateMovie, createMovie);
-
-router.delete('/movies/:movieId', auth, validateMovieId, deleteMovie);
+router.get('/movies', getMovies);
+router.post('/movies', validateMovie, createMovie);
+router.delete('/movies/:movieId', validateMovieId, deleteMovie);
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,10 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
-const checkPassword = require('../middlewares/password');
 
-const {
-  validateSignup, validateLogin, validateUser,
-} = require('../middlewares/validation');
+const { validateUser } = require('../middlewares/validation');
 
-const {
-  createUser, login, getCurrentUser, updateProfile,
-} = require('../controllers/users');
+const { getCurrentUser, updateProfile } = require('../controllers/users');
 
-router.post('/signup', validateSignup, checkPassword, createUser);
-router.post('/signin', validateLogin, checkPassword, login);
-
-router.get('/users/me', auth, getCurrentUser);
-router.patch('/users/me', auth, validateUser, updateProfile);
+router.get('/users/me', getCurrentUser);
+router.patch('/users/me', validateUser, updateProfile);
 
 module.exports = router;
